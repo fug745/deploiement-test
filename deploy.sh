20cd04144f4eae1a5268adf7f8f1a3679f6e2645
Sulliman Chetouani <sulliman.chetouanihajji@gmail.com>
2025-02-10 19:07:27 +0100

    Ajout du script de déploiement

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Déploiement en cours..."
+# Naviguer vers le dossier de l'application
+cd /home/ubuntu/app-test
+
+# Ajouter les commandes pour démarrer l'application
+echo "Application copiée et prête."
